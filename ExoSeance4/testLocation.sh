e827b285d9b66914c130d581c5c394603f129777
NgauvNicolas <ngauv.nicolas@gmail.com>
2023-10-15 22:37:54 +0200

    Mise à jour du Journal de Bord

--- ExoSeance4/testLocation.sh
+++ ExoSeance4/testLocation.sh
@@ -2,4 +2,6 @@
 
 # chemin="../ExoSeance1/ExerciceCours1/ann/"
 
+# $1 est l'année donnée en argumant à la commande en ligne de commande
+
 cat ../ExoSeance1/ExerciceCours1/ann/$1/* | grep Location | uniq | wc -l
