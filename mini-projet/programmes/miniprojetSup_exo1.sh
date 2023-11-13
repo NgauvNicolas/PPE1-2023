b157b577bd8f2c3f71abede8005003d7865439f0
NgauvNicolas <ngauv.nicolas@gmail.com>
2023-11-13 22:21:58 +0100

    Ajout des exercices 1 et 2 supplémentaires du miniprojet

---
+++ mini-projet/programmes/miniprojetSup_exo1.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Prend le 1er argument (le chemin vers le fichier texte) et le stocke dans la variable 'text'
+	# Par exemple, ici on a : ./../docs/candide.txt
+
+chemin_txt=$1
+
+# 'cat' affiche le contenu du fichier spécifié par 'chemin_txt'
+# 'tr' effectue plusieurs transformations sur le texte :
+	# - 'tr -d '[:punct:]'' supprime ponctuation
+	# - 'tr '[:upper:]' '[:lower:]'' convertit lettres majuscules en minuscules
+	# - 'tr ' ' '\n'' remplace espaces par sauts de ligne, on obtient alors des mots
+# 'grep -v '^$'' filtre lignes vides (mots vides) pour obtenir la liste des mots propres
+	# 'grep' -v inverser la mise en correspondance, pour sélectionner les lignes ne correspondant pas au motif
+cat "$chemin_txt" | tr -d '[:punct:]' | tr '[:upper:]' '[:lower:]' | tr ' ' '\n' | grep -v '^$'
---
+++ mini-projet/programmes/miniprojetSup_exo2.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# 1er argument (chemin vers fichier texte) et 2nd argument (nombre de mots) du script
+chemin_txt=$1
+nb_mots=$2
+
+# Chemin vers le script miniprojetSup_exo1.sh
+script="./miniprojetSup_exo1.sh"
+
+# Vérification du nb d'arguments donné au script : il en faut au moins 1
+if [ $# -lt 1 ]; then
+  echo "Format attendu : $script <source> [nombre_mots_souhaité]"
+  exit 1
+fi
+
+# Si le 2nd argument pas spécifié, définir valeur par défaut à 25
+if [ -z $nb_mots ]; then
+    nb_mots=25
+fi
+
+# Exécuter script miniprojetSup_exo1.sh avec fichier texte donné en argument et stocker résultat dans variable words
+mots=$($script $chemin_txt)
+
+# Trouver mots les plus fréquents en comptant occurrences, en les triant, en les regroupant et en prenant nb_mots premiers mots
+freq_mots=$(echo "$mots" | tr " " "\n" | sort | uniq -ci | sort -nr | head -$nb_mots)
+# Afficher résultat dans terminal pour voir
+echo "$freq_mots"
+
+# Sauvegarder les mots les plus courants dans un fichier "freq_mots.txt".
+echo "$freq_mots" > freq_mots.txt
