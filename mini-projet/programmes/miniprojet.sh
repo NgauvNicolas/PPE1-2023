329cd4d866e5c089740c1d0b15fd94e51380ef11
NgauvNicolas <ngauv.nicolas@gmail.com>
2023-11-17 22:18:42 +0100

    Mise à jour de la page d'accueil de présentation du mini-projet, du tableau du fichier HTML et du script qui le produit avec le style Bulma

--- mini-projet/programmes/miniprojet.sh
+++ mini-projet/programmes/miniprojet.sh
@@ -26,13 +26,16 @@ basename=$(basename -s .txt $chemin_urls)
 echo 	"<html>
 	<head>
 		<meta charset=\"UTF-8\" />
-		<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css\">
+		<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+		<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css\">
 		<title>Tableau des URLs</title>
 	</head>
 	<body>
-		<h1 class=\"title\">Tableau URLs $basename</h1>
-		<table class=\"table is-bordered\">
-			<thead><tr><th>Ligne</th><th>Code HTTP</th><th>URL</th><th>Encodage</th></thead>" > "../tableaux/$fic_tab"
+	<section>
+		<div class="container">
+			<h1 class=\"title\" style=\"text-align: center;\">Tableau URLs $basename</h1>
+			<table class=\"table is-bordered is-striped is-narrow is-hoverable\" style=\"margin: auto\">
+				<thead style=\"background-color: #b888e1;\"><tr><th style=\"color: #ffffff\">Ligne</th><th style=\"color: #ffffff\">Code HTTP</th><th style=\"color: #ffffff\">URL</th><th style=\"color: #ffffff\">Encodage</th></thead>" > "../tableaux/$fic_tab"
 # On pourra rajouter plus tard d'autres catégories au tableau, comme les aspirations, dumps, occurences, contextes, concordance, etc.S
 
 
@@ -71,3 +74,7 @@ do
 
 done < "$chemin_urls"
 # done < $chemin_urls
+
+echo "		</table>
+	</body>
+</html>" >> "$fic_tab"
